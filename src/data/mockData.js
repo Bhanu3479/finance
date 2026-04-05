6eac6e87dc62205c82768f621a9a3ed2f1cd6859
Bhanu3479 <bhanuprakash3479@gmail.com>
2026-04-05 19:43:28 +0530

    Update mockData.js

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,3 +1,4 @@
 export const transactionsData = [
-  { id: 1, date: "2026-04-01", category: "Salary", amount: 3000, type: "income" }
+  { id: 1, date: "2026-04-01", category: "Salary", amount: 3000, type: "income" },
+  { id: 2, date: "2026-04-04", category: "Food", amount: 1500, type: "expense" }
 ];
